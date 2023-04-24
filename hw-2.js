1812e76929634343dd9ab832929ee3c845ce2df1
Viktoria <v.bredikhina2000@mail.ru>
2023-04-24 11:02:24 +0300

    first 6 problems

---
+++ hw-2.js
@@ -0,0 +1,31 @@
+"use strict";
+// //Задание 1
+// let a=10; 
+// alert(a);
+// a=20;
+// alert(a);
+
+// //Задание 2
+// let iphoneRelease = 2007;
+// alert(iphoneRelease);
+
+// //Задание 3
+// let godGenius = "Брендан Эйх";
+// alert(godGenius);
+
+// //Задание 4
+// // let b=10;
+// // let c=2;
+// // alert(b+c);
+// // alert(b-c);
+// // alert(b/c);
+
+// //Задание 5
+
+// let result = 2**5;
+// alert(result);
+
+// // Задание 6
+// let m=9;
+// let p=5;
+// alert(9%5);
