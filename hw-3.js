cdeae403e7efa69990cafbee638114fa0da28180
Viktoria <v.bredikhina2000@mail.ru>
2023-04-29 22:01:28 +0300

    problem 7 added

--- hw-3.js
+++ hw-3.js
@@ -1,22 +1,88 @@
-// Задание 1
+/ Задание 1
 let password = 'пароль'
 let answer = prompt ('Введите пароль')
 answer.toLowerCase
-answer === password ? alert("Пароль введен верно") : ("Пароль введен неправильно");
-
+if(answer === password) {
+    alert('Введите пароль');
+} else {
+    alert('Пароль введен неправильно');
+}
 
 // Задание 2
-let c = 2
-c>0 && c<10? alert("Верно") : ("Неверно");
+let c = -3
+if(c>0 && c<10) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
 
 // Задание 3
-let d=50
-let e=200
-d>100 || e>100 ? alert('Верно') : alert('Неверно');
+let d=0
+let e=0
+if(d>100 || e>100) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
 
 // Задание 4
 let a = '2';
 let b = '3';
-// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+a = Number(a);
+b = Number (b);
 alert(a + b);
 
+// Задание 5
+let monthNumber = String(prompt('Введите номер месяц:'))
+switch (monthNumber) {
+
+case '12':
+alert('зима');
+break;
+case '1':
+alert('зима');
+break;
+case '2':
+alert('зима');
+break;
+case '43':
+alert('весна');
+break;
+case '4':
+alert('весна');
+break;
+case '5':
+alert('весна');
+break;
+case '6':
+alert('лето');
+break;
+case '7':
+alert('лето');
+break;
+case '8':
+alert('лето');
+break;
+case '9':
+alert('осень');
+break;
+case '10':
+alert('осень');
+break;
+case '11':
+alert('осень');
+break;
+
+default:
+    monthNumber>12
+    alert('Вы ввели что-то не то');
+    break;
+}
+
+// Задание 7
+const number = prompt("Пожалуйста, введите любое число: ");       
+if(number% 2 == 0){
+    alert("Это четное число");
+}
+else {    alert("Это нечетное число");
+} 
