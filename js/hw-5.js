ad17c91e50fb8620a1d24a242a3115c1147ae4a9
Viktoria <v.bredikhina2000@mail.ru>
2023-05-10 15:19:48 +0300

    problem 2 added

--- js/hw-5.js
+++ js/hw-5.js
@@ -1,11 +1,19 @@
 'use strict'
-// Задание 1
-function min(a, b) {
-    return a < b ? a : b;
-min (8,4);
-min (6,6);
-  }
+// // Задание 1
+// function min(a, b) {
+//     return a < b ? a : b;
+// min (8,4);
+// min (6,6);
+//   }
 // Задание 2
+function EvenUneven(a, answer) {
+  let a = Number (prompt('Пожалуйста, введите число'));
+  if (a % 2 == 0) {
+    alert(`Чётное`);
+  } else {
+    alert(`Нечётное`);
+  }
+}
 // Задание 3
 // Задание 4
 // Задание 5
