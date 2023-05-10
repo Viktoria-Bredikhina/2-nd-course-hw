ab7c04c4656a934523739fde99366578ccd67884
Viktoria <v.bredikhina2000@mail.ru>
2023-05-10 23:59:28 +0300

    problem 1 corrected

--- hw-5.js
+++ hw-5.js
@@ -1,10 +1,14 @@
 'use strict'
 // Задание 1
 function min(a, b) {
-    return a < b ? a : b;
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 min (8,4);
 min (6,6);
-  }
 // Задание 2
 function EvenUneven(a, answer) {
   let a = Number (prompt('Пожалуйста, введите число'));
