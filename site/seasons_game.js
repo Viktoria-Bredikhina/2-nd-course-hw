04222c6a7cec972825edea9b0e9cac0ae33a8eaf
Viktoria <v.bredikhina2000@mail.ru>
2023-05-11 00:28:27 +0300

    game changed

--- site/seasons_game.js
+++ site/seasons_game.js
@@ -1,30 +1,21 @@
 function findSeason()
 {
-    let month = String (prompt ('Введите номер месяца'));
-    switch (month) {
-    case 12:
-    case 1:
-    case 2:
-        alert(`Зима`);
-        break;
-    case 3:
-    case 4:
-    case 5:
-        alert(`Весна`);
-        break;
-    case 6:
-    case 7:
-    case 8:
-        alert(`Лето`);
-        break;
-    case 9:
-    case 10:
-    case 11:
-        alert(`Осень`);
-        break;
-    default:
-        alert(`Вы ввели что-то не то, попробуйте еще раз`);
-        break;
+    let month = Number (prompt ('Введите номер месяца'));
+    if (month) {
+        if (month===12 || month===1 || month===2) {
+            alert(`Зима`);
+        }
+  else if (month===3 || month===4 || month===5) {
+    alert(`Весна`);
+  }
+  else if (month===6 || month===7 || month===8) {
+    alert(`Лето`);
+  }
+  else if (month===9 || month===10 || month===11) {
+    alert(`Осень`);
+  }
+   else {
+    alert(`Вы ввели что-то не то, попробуйте еще раз`);}
     }
 }
 findSeason()
