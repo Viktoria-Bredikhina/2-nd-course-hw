ddaf21864ae33dc5a1100659bee97910eeaaee15
Viktoria <v.bredikhina2000@mail.ru>
2023-05-11 00:23:20 +0300

    problem 4 corrected

--- hw-5.js
+++ hw-5.js
@@ -36,21 +36,17 @@ figureSquare()
 // Задание 4
 function minorCheck() {
   let age = Number (prompt('Сколько Вам лет?'));
-  switch (age) {
-      case age>0 && age<12:
-      alert(`Привет, друг!`);
-      break;
-
-      case age>=13:
-      alert(`Добро пожаловать!`);
-      break;
-
-    default:
-      age<0;
-      alert(`Вы ввели неправильное значение`);
-      break;
+  if (age<0) {
+    alert(`Вы ввели неправильное значение`);
   }
-}
+  else if (age>0 && age<12) {
+    alert(`Привет, друг!`);
+  }
+   else if (age>=13) {
+    alert(`Добро пожаловать!`);
+   }
+
+  }
 minorCheck()
 // Задание 5
 function figureChecker() {
