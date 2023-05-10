a917f267454e1ab977b02e5c0620f46a0f7bea28
Viktoria <v.bredikhina2000@mail.ru>
2023-05-10 15:33:03 +0300

    problems 3 and 4 added

--- js/hw-5.js
+++ js/hw-5.js
@@ -1,10 +1,10 @@
 'use strict'
-// // Задание 1
-// function min(a, b) {
-//     return a < b ? a : b;
-// min (8,4);
-// min (6,6);
-//   }
+// Задание 1
+function min(a, b) {
+    return a < b ? a : b;
+min (8,4);
+min (6,6);
+  }
 // Задание 2
 function EvenUneven(a, answer) {
   let a = Number (prompt('Пожалуйста, введите число'));
@@ -14,8 +14,34 @@ function EvenUneven(a, answer) {
     alert(`Нечётное`);
   }
 }
-// Задание 3
+// Задание 3.1
+function figureSquare(a, b) {
+  let a = Number (prompt('Введите число'));
+  b = a**2;
+  console.log('${b}');
+}
+// Задание 3.2
+function figureSquare(a, b) {
+  let a = Number (prompt('Введите число'));
+  returen b = a**2;
+  console.log('${b}');
+}
 // Задание 4
+function minorCheck(age, answer) {
+  let age = String (prompt('Сколько Вам лет?'));
+  switch (age) {
+      case a>0 && a<12;
+      alert(`Привет, друг!`);
+      break;
+
+      case a>=13;
+      alert(`Добро пожаловать!`);
+    default:
+      a<0;
+      alert(`Вы ввели неправильное значение`);
+      break;
+  }
+}
 // Задание 5
 // Задание 6
 // Задание 7
