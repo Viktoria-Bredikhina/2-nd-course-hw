a7b2865dc60907faae867c92bd479f71202a066e
Viktoria <v.bredikhina2000@mail.ru>
2023-05-15 23:37:59 +0300

    regroupping

---
+++ hw-6.js
@@ -0,0 +1,24 @@
+'use strict'
+//Задание 1
+const numbs = [1, 5, 4, 10, 0, 3];
+for (let i=0; i<numbs.length; i++) {if (numbs[i]==10) break;
+console.log(numbs[i]);};
+//Задание 2
+//Задание 3
+let random = [1, 3, 5, 10, 20];
+let random = () random.split('');
+console.log(random);
+//Задание 4
+//Задание 5
+//Задание 6
+//Задание 7
+let array = [9, 8, 7, 6, 5];
+let index = (prompt('Введите число от 1 до 10'));
+let search = array.includes(index);
+console.log(search);
+//Задание 8
+let message = 'abcdef';
+a = message.reverse();
+console.log(a);
+//Задание 9
+//Задание 10
