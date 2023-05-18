0a364b8e3c8474f8cf55974f0863cad6956653ec
Viktoria <v.bredikhina2000@mail.ru>
2023-05-18 11:05:23 +0300

    pr 1 added

---
+++ hw-7.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Задание 1
+let str = 'js'
+console.log(str.toUpperCase());
+
+//Задание 2
+
+
+const array = [собака, кошка, бегемот, корова];
+const newArray = array.filter( initial => age > 18 );
+let initial = 'ко';
+// Массив ableToDrink будет содержать два значения: [19, 21]
+
