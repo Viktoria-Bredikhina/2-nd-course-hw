2d47f872f017577475f8a4da915faa8ab9f89f80
Viktoria <v.bredikhina2000@mail.ru>
2023-05-20 15:04:59 +0300

    problems 1-8 added

--- hw-7.js
+++ hw-7.js
@@ -6,9 +6,65 @@ console.log(str.toUpperCase());
 
 //Задание 2
 
+const words = ['кошка', 'бегемот', 'корова', 'берег'];
 
-const array = [собака, кошка, бегемот, корова];
-const newArray = array.filter( initial => age > 18 );
-let initial = 'ко';
-// Массив ableToDrink будет содержать два значения: [19, 21]
+const filterItems = (arr, query) => {
+  return arr.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+}
+console.log(filterItems(words, 'ко')); 
+console.log(filterItems(words, 'бе'));
+  
+//Задание 3
+let a = 32.58884;
+// 1
+console.log(Math.floor(a));
+// 2
+console.log(Math.ceil(a));
+// 3 
+// 2
+console.log(Math.round(a));
 
+// Задание 4. Даны числа 52, 53, 49, 77, 21, 32. найти среди этих чисел наименьшее и наибольшее числа и вывести их в консоль.
+
+let figures = [52, 53, 49, 77, 21, 32];
+console.log(Math.min(figures))
+console.log(Math.max(figures))
+
+//Задание 5. Создайте функцию, которая будет выводить в консоль рандомное число от 1 до 10.
+
+let figuresRandomizer = (min, max) => {
+  console.log(Math.random())
+}
+figuresRandomizer(1, 10);
+
+// Задание 6. Напишите функцию, которая будет принимать на вход целое число, а возвращать массив случайных целых чисел от 0 до переданного числа. Длина массива должна быть в 2 раза меньше переданного числа.
+
+let arrCreator = (num) => {
+  const randomRow = [];
+  randomRow.length = Math.floor(num/2);
+  for(let i = 0; i < randomRow.length; i++) {
+randomRow.push(Math.floor(Math.random) * 5)
+  }
+ 
+}
+
+// Задание 7. Напишите функцию, которая на вход принимает 2 целых числа, а в качестве результата возвращает случайное целое число в этом диапазоне.
+function generateRandom(min = 0, max = 100) {
+
+  let difference = max - min;
+  let rand = Math.random();
+  rand = Math.floor( rand * difference);
+  rand = rand + min;
+  return rand;
+}
+console.log(generateRandom(2,6));
+// Задание 8. Выведите в консоль текущую дату в стандартном режиме. Используйте один из трех рассмотренных в уроке способов.
+
+let currentDate = new Date('20 May 2023');
+console.log(currentDate);
+
+// Задание 9. Создайте переменную currentDate и сохраните в нее текущую дату. Выведите дату, которая наступит через 73 дня после текущей.
+
+// Задание 10.
+
+// Задание 11.
