a73a0332d0b6a74ad78553df565c0c94a82c788f
Viktoria <v.bredikhina2000@mail.ru>
2023-05-21 16:50:08 +0300

    script connected

--- site/guess_the_word.js
+++ site/guess_the_word.js
@@ -4,18 +4,18 @@ function memorizeWords() {
     let fruitsArraySort = fruitsArray.sort(() => Math.random() - 0.5);
     alert(fruitsArraySort);
 
-    let indexWords = fruitsArraySort.length - 1;
+    let lastIntheArray = fruitsArraySort.length - 1;
 
     let userAnswerFirstEl = prompt('Чему равнялся первый элемент массива?');
     let userAnswerLastEl = prompt('Чему равнялся последний элемент массива?');
 
     if (userAnswerFirstEl.toLowerCase() === fruitsArraySort[0].toLowerCase() && 
-    userAnswerLastEl.toLowerCase() === fruitsArraySort[indexWords].toLowerCase()) {
+    userAnswerLastEl.toLowerCase() === fruitsArraySort[lastIntheArray].toLowerCase()) {
 
         alert('Поздравляем, Вы угадали оба элемента!');
 
     } else if (userAnswerFirstEl.toLowerCase() === fruitsArraySort[0].toLowerCase() || 
-    userAnswerLastEl.toLowerCase() === fruitsArraySort[indexWords].toLowerCase()) {
+    userAnswerLastEl.toLowerCase() === fruitsArraySort[lastIntheArray].toLowerCase()) {
 
         alert ('Вы были близки к победе!');
 
