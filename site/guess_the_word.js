19a946068d0c3dbea8226fb48abc42a7fa7d7a5d
Viktoria <v.bredikhina2000@mail.ru>
2023-05-21 16:47:11 +0300

    guess the word game completed

--- site/guess_the_word.js
+++ site/guess_the_word.js
@@ -1,12 +1,26 @@
-function wordRandomizer () {
-    let arr = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
-alert(newAarr = arr.sort(() => Math.random() - 0.5));
-let firstElement = String (prompt('Чему равнялся первый элемент массива?'));
-firstElement.toLowerCase
-if (firstElement === newArr[0]){
-    alert(`Ура! Ты молодец!`)}
-    else {
-        alert(`Простите, но вы ответили неверно!`)}
+function memorizeWords() {
+
+    const fruitsArray = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    let fruitsArraySort = fruitsArray.sort(() => Math.random() - 0.5);
+    alert(fruitsArraySort);
+
+    let indexWords = fruitsArraySort.length - 1;
+
+    let userAnswerFirstEl = prompt('Чему равнялся первый элемент массива?');
+    let userAnswerLastEl = prompt('Чему равнялся последний элемент массива?');
+
+    if (userAnswerFirstEl.toLowerCase() === fruitsArraySort[0].toLowerCase() && 
+    userAnswerLastEl.toLowerCase() === fruitsArraySort[indexWords].toLowerCase()) {
+
+        alert('Поздравляем, Вы угадали оба элемента!');
+
+    } else if (userAnswerFirstEl.toLowerCase() === fruitsArraySort[0].toLowerCase() || 
+    userAnswerLastEl.toLowerCase() === fruitsArraySort[indexWords].toLowerCase()) {
+
+        alert ('Вы были близки к победе!');
+
+    } else {
+        alert ('К сожалению, вы ответили неверно.')
     }
 
-
+}
