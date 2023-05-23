d0476718415da13ab20faaaf394572f9ef148cd0
Viktoria <v.bredikhina2000@mail.ru>
2023-05-23 22:55:23 +0300

    task 1 added

---
+++ hw-8.js
@@ -0,0 +1,21 @@
+//Задание 1
+
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+
+ function sortAge(a, b) {
+     return a.age - b.age;
+ };
+ console.log(people.sort(sortAge));
+
+//Задание 2
+
+//Задание 3
+
+//Задание 4
+
+//Задание 5
