849d82f79a4eb99b1d6299f4333431e8c0820373
Viktoria <v.bredikhina2000@mail.ru>
2023-05-24 22:56:33 +0300

    1-5 added

--- hw-8.js
+++ hw-8.js
@@ -14,8 +14,68 @@ const people = [
 
 //Задание 2
 
+ function isPositive(number) {
+     return number > 0;
+ }
+  function isMale(word) {
+     return word.gender === 'male';
+ }
+ function filter(arr, ruleFunction) {
+
+     const arrayNew = [];
+
+     for (let i = 0; i < arr.length; i++) {
+         if (ruleFunction(arr[i])) {
+             arrayNew.push(arr[i]);
+         }
+     }
+
+
+     return arrayNew;
+
+ }
 //Задание 3
+
+
+ function getCurrentDate(time, timeStop) {
+     let currentDateInterval = setInterval(() => {
+         let currentDate = new Date();
+         console.log(currentDate);
+     }, time);
+     let stopInterval = setTimeout(() => {
+         clearInterval(currentDateInterval);
+         console.log('30 секунд прошло');
+     }, timeStop);
+
+ }
+ getCurrentDate(3000, 30000);
 
 //Задание 4
 
+function delayForSecond(callback) {
+ 	setTimeout(() => {
+         console.log (callback());
+     }, 1000);
+ }
+
+ delayForSecond(function () {
+    console.log('Привет, Глеб!');
+ })
+
 //Задание 5
+
+function delayForSecond(cb) {
+     setTimeout(() => {
+         console.log('Прошла одна секунда');
+         if (cb) { cb(); }
+
+     }, 1000)
+ }
+
+ function sayHi(name) {
+     console.log(`Привет, ${name}!`);
+ }
+
+ delayForSecond(function () {
+     sayHi('Глеб')
+ });
